388746de9aa8fd7c7dfed6833d566d1d6b4c761a
Student <example@example.com>
2019-11-19 09:39:09 -0800

    add redirect to home after form submission

--- src/components/AddFriend.jsx
+++ src/components/AddFriend.jsx
@@ -3,58 +3,71 @@ import PropTypes from 'prop-types';
 import { v1 } from 'uuid';
 import { Redirect } from 'react-router-dom';
 
-function AddFriend(props) {
-  let _name = null;
-  let _tweet = null;
-  let _imgURL = null;
+class AddFriend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      _tweet: null,
+      _name: null,
+      _imgURL: null,
+      toHome: false
+    }
+    this.handleAddFriendSubmission = this.handleAddFriendSubmission.bind(this);
+  }
 
-  function handleAddFriendSubmission(event) {
+  handleAddFriendSubmission(event) {
+    console.log(this.state._name.value);
     event.preventDefault();
-    console.log(_name.value);
-    props.onNewFriendCreation({name: _name.value, tweet: _tweet.value, imgURL: _imgURL.value, id: v1()});
-    _name.value = '';
-    _tweet.value = '';
-    _imgURL.value = '';
-   }
-  
-  var formStyle = {
+    this.props.onNewFriendCreation({ name: this.state._name.value, tweet: this.state._tweet.value, imgURL: this.state._imgURL.value, id: v1() });
+    this.state._name.value = '';
+    this.state._tweet.value = '';
+    this.state._imgURL.value = '';
+    this.setState({ toHome: true })
+  }
+
+  render() {
+    var formStyle = {
       backgroundColor: '#537ec5'
-  }
-
-  var btnStyle = {
-    backgroundColor: '#f39422',
-    float: 'right',
-    marginTop: '10px',
-    marginLeft: '3px',
-    textDecoration: 'none'
-  }
-
-  var inputStyle = {
+    }
+    var btnStyle = {
+      backgroundColor: '#f39422',
+      float: 'right',
+      marginTop: '10px',
+      marginLeft: '3px',
+      textDecoration: 'none'
+    }
+    var inputStyle = {
       paddingLeft: '15px'
-  }
-  return (
-    <div style={formStyle}>
-      <form style={inputStyle} onSubmit={handleAddFriendSubmission}>
-        <input 
-          type='text'
-          id='name'
-          placeholder='Name'
-          ref={(input) => {_name = input;}}/>
-        <input
-          type='text'
-          id='tweet'
-          placeholder='Quote'
-          ref={(input) => {_tweet = input;}}/>
-        <input
-          type='text'
-          id='imgURL'
-          placeholder='Image URL'
-          ref={(input) => {_imgURL = input;}}/>
+    }
+
+    if (this.state.toHome === true) {
+      return <Redirect to='/' />
+    }
+
+    return (
+      <div style={formStyle}>
+        <form style={inputStyle} onSubmit={this.handleAddFriendSubmission}>
+          <input
+            type='text'
+            id='name'
+            placeholder='Name'
+            ref={(input) => { this.state._name = input; }} />
+          <input
+            type='text'
+            id='tweet'
+            placeholder='Quote'
+            ref={(input) => { this.state._tweet = input; }} />
+          <input
+            type='text'
+            id='imgURL'
+            placeholder='Image URL'
+            ref={(input) => { this.state._imgURL = input; }} />
           <button className='btn' style={btnStyle}><a target="_blank" href='https://www.google.com/search?q=nicolas+cage&rlz=1C5CHFA_enUS876&source=lnms&tbm=isch&sa=X&ved=0ahUKEwiT-aiw5vTlAhW5JjQIHZ_CDR8Q_AUIEygC&biw=2080&bih=1288'>Upload Photo</a></button>
-        <button className='btn' style={btnStyle} type='submit'>Add</button>
-      </form>
-    </div>
-  );
+          <button className='btn' style={btnStyle} type='submit'>Add</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 AddFriend.propTypes = {
---
+++ src/components/AddFriendControl.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+
+class AddFriendControl extends React.Component() {
+    constructor() {
+        super(props);
+        this.state = {
+            formVisibleOnPage: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    var currentComponent = null;
+
+    //update local on form submission
+
+
+  
+  //render conditionally based on local state
+  return (
+
+  );
+}
+
+
+export default AddFriendControl;
